0646ee15a637e8235ffa2c09b1587e5d785fc892
Aaron <impre0hn@gmail.com>
2026-08-29 12:08:21 -0600

    miracle: conexion con la base de datos

--- Java/ProyectoTeo/src/main/java/com/aaron/proyectoteo/ProyectoTeo.java
+++ Java/ProyectoTeo/src/main/java/com/aaron/proyectoteo/ProyectoTeo.java
@@ -11,6 +11,6 @@ package com.aaron.proyectoteo;
 public class ProyectoTeo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new connect().setVisible(true);
     }
 }
